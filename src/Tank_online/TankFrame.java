077fe5dcbfa9b8476ae2a4e9d715b9d19f3bd366
xhuanc <756258495@qq.com>
2021-04-16 16:14:25 +0800

    添加Key监听器
    自定义类实现Keylistener接口

--- src/Tank_online/TankFrame.java
+++ src/Tank_online/TankFrame.java
@@ -1,6 +1,8 @@
 package Tank_online;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -17,6 +19,7 @@ public class TankFrame extends Frame {
                 System.exit(0);
             }
         });
+        addKeyListener(new MyKeyListener());
         setVisible(true);
     }
 
@@ -24,7 +27,36 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
         g.setColor(Color.black);
         g.fillRect(x,y,50,50);
-        x+=10;
-        y+=10;
+    }
+    //键盘监听器
+    class MyKeyListener implements KeyListener{
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            char key=e.getKeyChar();
+            switch (key)
+            {
+                case 'w':y-=20;break;
+                case 'a':x-=20;break;
+                case 's':y+=10;break;
+                case 'd':x+=10;break;
+            }
+            repaint();
+            char lastkey=key;
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key=e.getKeyCode();
+            if(key=='A')
+            {
+                repaint();
+            }
+        }
     }
 }
